118d5325501b64f57d8ce7189ab0b6b973b0f1e8
Taciana3090 <taciana3090@gmail.com>
2022-07-21 18:32:18 -0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+# importando as bibliotecas
 import streamlit as st
+import os 
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import plotly.express as px
+import numpy as np
+from PIL import Image
 
-st.set_page_config(
-    page_title = "Teste do Streamlit G4",
-    layout = "wide",
-    menu_items = {
-        'About': "TESTE DO ABOUT"
-    }
-)
 
+# Adicionando um titulo
+st.title('Gráficos e opções de filtragem')
+
+# importando conjunto de dados
+df = pd.read_csv('https://github.com/Taciana3090/Bbooks/raw/master/data/dados.csv', delimiter=',') # --> caminho para o arquivo com os dados  
+
+
+# Padronização dos nomes
+orig_cols = list(df.columns) 
+new_cols = [] 
+for col in orig_cols:     
+    new_cols.append(col.strip().replace('  ', ' ').replace(' ', '_').lower())
